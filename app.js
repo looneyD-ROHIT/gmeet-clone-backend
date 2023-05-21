570aee9783a2669569d0c82a04251c0a2dcd4d3d
ROHIT SADHU <rohitsadhupersonal@gmail.com>
2023-05-21 20:54:19 +0530

    +video calling works

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ import logout from './routes/logout.js';
 import ping from './routes/ping.js';
 import errorHandler from './middleware/errorHandler.js'
 import allowedOrigins from "./config/allowedOrigins.js";
+import { PeerServer } from 'peer';
 
 
 const app = express();
@@ -76,8 +77,6 @@ app.use('/logout', verifyJWT, logout);
 // hidden route requiring authentication --> to check user login status when idle
 app.use('/ping', verifyJWT, ping);
 
-// fallback route
-app.all('*', fallback);
 
 // middleware to handle errors
 app.use(errorHandler);
@@ -87,6 +86,12 @@ const server = http.createServer(app);
 
 // socket initialisation
 socketInitialisation(server);
+
+// peer server initialization
+const peerServer = PeerServer({ port: 6969, path: "/peerjs", debug: true });
+
+// fallback route
+app.all('*', fallback);
 
 const PORT = 9000 || process.env.PORT;
 
--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -12,7 +12,7 @@ const allowedOrigins = [
     'https://admin.socket.io/',
     'https://meetclone.vercel.app',
     'https://meetclonetest.vercel.app',
-    'https://5626-45-116-190-225.ngrok-free.app',
+    'https://mytestapp-two.vercel.app/',
 ];
 
 export default allowedOrigins;
